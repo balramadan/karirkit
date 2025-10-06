d282118867d14ffa0bb82c44c8be9495834ac8dd
Iqbal Ramadan <iqbalramadan3130@gmail.com>
2025-10-06 22:38:22 +0700

    Change config vercel and use cache mongoose

--- api/lib/db.js
+++ api/lib/db.js
@@ -1,8 +1,28 @@
 import mongoose from "mongoose";
 
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections from growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function connectDB() {
-  await mongoose.connect(process.env.MONGODB_URI, { dbName: "karirkit" });
-  console.log("MongoDB connected");
+  if (cached.conn) {
+    console.log("MongoDB using cached connection");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, { dbName: "karirkit", bufferCommands: false }).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  console.log("MongoDB connected (new connection)");
+  return cached.conn;
 }
 
 export default connectDB;
