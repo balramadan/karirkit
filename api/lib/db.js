f1432be85915a853e950b3799331efc470a7b007
Iqbal Ramadan <iqbalramadan3130@gmail.com>
2025-10-07 16:55:24 +0700

    Change the deployment method

--- api/lib/db.js
+++ api/lib/db.js
@@ -1,28 +1,8 @@
 import mongoose from "mongoose";
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections from growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 async function connectDB() {
-  if (cached.conn) {
-    console.log("MongoDB using cached connection");
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, { dbName: "karirkit", bufferCommands: false }).then((mongoose) => mongoose);
-  }
-  cached.conn = await cached.promise;
-  console.log("MongoDB connected (new connection)");
-  return cached.conn;
+  await mongoose.connect(process.env.MONGODB_URI, { dbName: "karirkit" });
+  console.log("MongoDB connected");
 }
 
 export default connectDB;
